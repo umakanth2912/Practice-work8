cf2d718d386d0708ab1d4916654c5db44e05b761
umaka <umaka@DESKTOP-G3O80KM>
2017-02-20 23:54:28 -0600

    Add project files.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] RiceItems = { "1.Plain Rice", "2.Chicken Biryani", "3.Mutton Biryani\n" };
+            string[] CurryItems = { "1.Tomato Curry", "2.Chicken Curry", "3.Mutton Curry" };
+            StringBuilder strbl = new StringBuilder("-*-*-*-*-*-*-*-* Menu -*-*-*-*-*-*-*-*\n\n\n");
+            strbl.AppendLine("\t*****Rice Items*****");
+            foreach(string items in RiceItems)
+            {
+                strbl.Append(items).AppendLine();
+            }
+
+            strbl.AppendLine("\t*****Curry Items*****");
+            foreach (string items in CurryItems)
+            {
+                strbl.Append(items).AppendLine();
+            }
+            Console.WriteLine(strbl.ToString());
+            Console.ReadLine();
+            
+        }
+    }
+}
